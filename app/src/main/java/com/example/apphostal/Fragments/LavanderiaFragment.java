fec23a8eea35e86b803ebacb0631b866f1f34751
Juan Carlos <jcs1011@gmail.com>
2024-06-06 13:56:46 +0200

    se actualiza LavanderiaFragment

--- app/src/main/java/com/example/apphostal/Fragments/LavanderiaFragment.java
+++ app/src/main/java/com/example/apphostal/Fragments/LavanderiaFragment.java
@@ -9,12 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.apphostal.Clases.EditTextFocusHelper;
 import com.example.apphostal.R;
 
 public class LavanderiaFragment extends Fragment {
+    private EditText editTextFecha,edbajeras,edencimeras,edfundalomohada,edprotectora,ednordica,edcolchav,edtoallaD,edtoallaL,edalfombrim,edpaid,edprotectC;
 
-   private Button btnMenu;
+   private Button btnMenu, btnEnviarDatos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +27,22 @@ public class LavanderiaFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_lavanderia, container, false);
         // Inicializar EditTexts y Botones
         btnMenu = view.findViewById(R.id.btnMenu);
+        btnEnviarDatos = view.findViewById(R.id.btnEnviarDatos);
+
+        // Inicializar los EditText
+
+        editTextFecha = view.findViewById(R.id.editTextFecha);
+        edbajeras = view.findViewById(R.id.edbajeras);
+        edencimeras = view.findViewById(R.id.edencimeras);
+        edfundalomohada = view.findViewById(R.id.edfundalomohada);
+        edprotectora = view.findViewById(R.id.edprotectora);
+        ednordica = view.findViewById(R.id.ednordica);
+        edcolchav = view.findViewById(R.id.edcolchav);
+        edtoallaD = view.findViewById(R.id.edtoallaD);
+        edtoallaL = view.findViewById(R.id.edtoallaL);
+        edalfombrim = view.findViewById(R.id.edalfombrim);
+        edpaid = view.findViewById(R.id.paid);
+        edprotectC = view.findViewById(R.id.protectC);
 
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +52,36 @@ public class LavanderiaFragment extends Fragment {
                 fragmentManager.beginTransaction().remove(LavanderiaFragment.this).commit();
             }
         });
+
+        btnEnviarDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Verificar si algún EditText está vacío
+                if (EditTextFocusHelper.isAnyEditTextEmpty(editTextFecha,edbajeras,edencimeras,edfundalomohada,edprotectora,ednordica,edtoallaD,edtoallaL, edalfombrim,edpaid,edprotectC/* otros EditText */)) {
+                    // Mostrar mensaje de error
+                    Toast.makeText(getContext(), "Por favor, completa todos los campos.", Toast.LENGTH_SHORT).show();
+
+                    // Guardar los datos
+                } else {
+                    //enviarDatos();
+                }
+
+            }
+        });
+
+        // Llama a EditTextFocusHelper.setupEditTextFocus() para configurar el comportamiento del EditText deseado
+        EditTextFocusHelper.setupEditTextFocus(edbajeras);
+        EditTextFocusHelper.setupEditTextFocus(edencimeras);
+        EditTextFocusHelper.setupEditTextFocus(edfundalomohada);
+        EditTextFocusHelper.setupEditTextFocus(edprotectora);
+        EditTextFocusHelper.setupEditTextFocus(ednordica);
+        EditTextFocusHelper.setupEditTextFocus(edcolchav);
+        EditTextFocusHelper.setupEditTextFocus(edtoallaD);
+        EditTextFocusHelper.setupEditTextFocus(edtoallaL);
+        EditTextFocusHelper.setupEditTextFocus(edalfombrim);
+        EditTextFocusHelper.setupEditTextFocus(edpaid);
+        EditTextFocusHelper.setupEditTextFocus(edprotectC);
+
         return view;
 
 
